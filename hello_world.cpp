3861bd093c89c3d56dec14ee1a22eb65c40fb71e
Jonathan Roosa <JonathanRoosa@my.unt.edu>
2016-03-26 19:20:21 -0500

    Add multiple hello world statements and comment for merge collision testing.

--- hello_world.cpp
+++ hello_world.cpp
@@ -3,7 +3,11 @@ using namespace std;
 
 int main() {
 	cout << "Hello world!" << endl;
-	cout<<" Evan was here"<< endl;
-	// and made some changes
+	cout << "Hello world 2!" << endl;
+	cout << "Hello world 3!" << endl;
+
+
+
+	// Comment
 	return 0;
 }
